e0c4233d21f9799350e3f4582d7145851785d594
ORER <76395021+junseoksb@users.noreply.github.com>
2021-01-15 23:00:28 +0900

    Update presetscript.js

--- presetscript.js
+++ presetscript.js
@@ -13,3 +13,19 @@ function toggleimg3() {
 function toggleimg4() {
     document.getElementById("photo4").src = "Preset Lab/after4.JPG";
 }
+
+function toggleimg01() {
+    document.getElementById("photo1").src = "Preset Lab/photo1.JPG";
+}
+
+function toggleimg02() {
+    document.getElementById("photo2").src = "Preset Lab/photo2.JPG";
+}
+
+function toggleimg03() {
+    document.getElementById("photo3").src = "Preset Lab/photo3.JPG";
+}
+
+function toggleimg04() {
+    document.getElementById("photo4").src = "Preset Lab/photo4.JPG";
+}
